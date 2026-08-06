81a701205da9ff28d18c12f0e25eb023af645ed5
Njål Gaute Solland <njaal.solland@gmail.com>
2026-08-06 06:59:50 +0200

    feat: export Veritas integrity errors

--- src/veritas/__init__.py
+++ src/veritas/__init__.py
@@ -7,14 +7,20 @@ from veritas.contracts import (
     ObservationPackageV1,
     StoredEvidenceReportV1,
 )
-from veritas.digest import CANONICALIZATION_ALGORITHM, canonical_digest, stable_json
+from veritas.digest import (
+    CANONICALIZATION_ALGORITHM,
+    CanonicalizationError,
+    canonical_digest,
+    stable_json,
+)
 from veritas.service import VeritasChainError, VeritasChainService
-from veritas.worm import WORMLog
+from veritas.worm import WORMIntegrityError, WORMLog
 
 __version__ = "0.1.0"
 
 __all__ = [
     "CANONICALIZATION_ALGORITHM",
+    "CanonicalizationError",
     "CompletedEvidencePackageV1",
     "FinalEvidenceBindingV1",
     "ObservedEventV1",
@@ -22,6 +28,7 @@ __all__ = [
     "StoredEvidenceReportV1",
     "VeritasChainError",
     "VeritasChainService",
+    "WORMIntegrityError",
     "WORMLog",
     "canonical_digest",
     "stable_json",
